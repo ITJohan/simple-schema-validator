e9582b7a982a1661b4ac9cd58cf17fe7bbf5eeac
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-05 18:38:43 +0200

    Export types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export * from "./src/types.js";
 export { boolean } from "./src/primitives/boolean.js";
 export { number } from "./src/primitives/number.js";
 export { object } from "./src/primitives/object.js";
