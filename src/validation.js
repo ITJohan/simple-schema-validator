4b79066bd5e713f0328df5b6e4c8a847364e0368
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-10 09:31:48 +0200

    Function factory based Validation

---
+++ src/validation.js
@@ -0,0 +1,90 @@
+/**
+ * @typedef {object} ValidationError
+ * @prop {string} message
+ * @prop {string} [property]
+ * @prop {any} value
+ */
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @typedef {object} Success
+ * @prop {"success"} tag
+ * @prop {A} value
+ * @prop {<B>(fn: (x: A) => B) => Validation<E, B>} map
+ * @prop {<B>(other: Validation<E, (x: A) => B>) => Validation<E, B>} ap
+ * @prop {<B>(onFailure: (errors: E[]) => B, onSuccess: (value: A) => B) => B} fold
+ */
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @typedef {object} Failure
+ * @prop {"failure"} tag
+ * @prop {E[]} errors
+ * @prop {<B>(fn: (x: A) => B) => Validation<E, B>} map
+ * @prop {<B>(other: Validation<E, (x: A) => B>) => Validation<E, B>} ap
+ * @prop {<B>(onFailure: (errors: E[]) => B, onSuccess: (value: A) => B) => B} fold
+ */
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @typedef {Success<E, A> | Failure<E, A>} Validation
+ */
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @param {A} x
+ * @returns {Validation<E, A>}
+ */
+const success = (x) => ({
+  tag: "success",
+  value: x,
+  map: (fn) => success(fn(x)),
+  ap: (other) => {
+    if (other.tag === "failure") {
+      return failure(other.errors);
+    }
+    return success(other.value(x));
+  },
+  fold: (_onFailure, onSuccess) => onSuccess(x),
+});
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @param {E[]} x
+ * @returns {Validation<E, A>}
+ */
+const failure = (x) => {
+  /** @type {Validation<E, any>} */
+  const instance = {
+    tag: "failure",
+    errors: x,
+    map: () => {
+      return instance;
+    },
+    ap: (other) => {
+      if (other.tag === "failure") {
+        return failure([...x, ...other.errors]);
+      }
+      return instance;
+    },
+    fold: (onFailure, _onSuccess) => onFailure(x),
+  };
+  return instance;
+};
+
+/**
+ * @template {ValidationError} E
+ * @template A
+ * @param {A} x
+ * @returns {Validation<E, A>}
+ */
+const of = (x) => success(x);
+
+const validation = { success, failure, of };
+
+export { validation };
---
+++ src/validation.test.js
@@ -0,0 +1,114 @@
+/** @import { Validation, ValidationError } from "./validation.js" */
+
+import { describe, it } from "@std/testing/bdd";
+import { validation } from "./validation.js";
+import { assertEquals } from "@std/assert";
+
+/**
+ * @param {any} num
+ * @returns {Validation<ValidationError, number>}
+ */
+const validateNum = (num) =>
+  num >= 18
+    ? validation.success(num)
+    : validation.failure([{ message: "Too low", value: num }]);
+
+describe("validation", () => {
+  describe(validation.success.name, () => {
+    it("should return a Validation<E, A> that has a tag = success property", () => {
+      assertEquals(validation.success(123).tag, "success");
+    });
+
+    it("should return a Validation<E, A> that has a value = A property", () => {
+      const result = validation.success(123);
+      assertEquals(result.tag === "success" && result.value, 123);
+    });
+
+    it("should return a Validation<E, A> that can map to Validation<E, B>", () => {
+      assertEquals(
+        validation.success(123).map((num) => String(num)).fold(
+          (err) => String(err),
+          (val) => val,
+        ),
+        "123",
+      );
+    });
+
+    it("should return a Validation<E, A> that can apply to Validation<E, B>", () => {
+      assertEquals(
+        validation.success(123).ap(validation.of(validateNum)).fold(
+          (err) => Number(err),
+          (val) => val.tag === "success" && val.value,
+        ),
+        123,
+      );
+    });
+
+    it("should return a Validation<E, A> that can fold to B", () => {
+      assertEquals(
+        validation.success(123).fold(
+          (err) => Number(err),
+          (val) => val,
+        ),
+        123,
+      );
+    });
+  });
+
+  describe(validation.failure.name, () => {
+    it("should return a Validation<E, A> that has a tag = failure property", () => {
+      assertEquals(
+        validation.failure([{ message: "Error", value: 123 }]).tag,
+        "failure",
+      );
+    });
+
+    it("should return a Validation<E, A> that has a errors = E property", () => {
+      const result = validation.failure([{ message: "Error", value: 123 }]);
+      assertEquals(result.tag === "failure" && result.errors, [{
+        message: "Error",
+        value: 123,
+      }]);
+    });
+
+    it("should return a Validation<E, A> that maps to Validation<E, A>", () => {
+      assertEquals(
+        validation.failure([{ message: "Error", value: 123 }]).map((num) =>
+          String(num)
+        ).fold(
+          (err) => err[0].message,
+          (val) => val,
+        ),
+        "Error",
+      );
+    });
+
+    it("should return a Validation<E, A> that apply to Validation<E, A>", () => {
+      assertEquals(
+        validation.failure([{ message: "Error", value: 123 }]).ap(
+          validation.of(validateNum),
+        ).fold(
+          (err) => err[0].message,
+          (val) => val.tag,
+        ),
+        "Error",
+      );
+    });
+
+    it("should return a Validation<E, A> that can fold to B", () => {
+      assertEquals(
+        validation.failure([{ message: "Error", value: 123 }]).fold(
+          (err) => err[0].message,
+          (val) => val,
+        ),
+        "Error",
+      );
+    });
+  });
+
+  describe(validation.of.name, () => {
+    it("returns a Success", () => {
+      assertEquals(validation.of(123).tag, "success");
+    });
+  });
+});
