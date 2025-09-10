6562c2faaa8f1d022456674f98a7a9c7a720c446
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-10 13:19:07 +0200

    Export object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export * from "./src/types.js";
 export { boolean } from "./src/schemas/boolean.js";
+export { email } from "./src/schemas/email.js";
 export { number } from "./src/schemas/number.js";
 export { string } from "./src/schemas/string.js";
-export { email } from "./src/schemas/email.js";
+export { object } from "./src/schemas/object.js";
 export { positive } from "./src/schemas/positive.js";
