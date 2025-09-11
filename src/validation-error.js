a854cc4bcbb837e6c6d2118964cac9313d04f5c0
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-11 14:25:38 +0200

    Add back validation error class

---
+++ src/validation-error.js
@@ -0,0 +1,18 @@
+class ValidationError extends Error {
+  /**
+   * @param {{
+   *  message?: string;
+   *  key?: string;
+   *  value: any;
+   *  options?: ErrorOptions;
+   * }} params
+   */
+  constructor({ message, key, value, options }) {
+    super(message, options);
+    this.name = "ValidationError";
+    this.key = key;
+    this.value = value;
+  }
+}
+
+export { ValidationError };
