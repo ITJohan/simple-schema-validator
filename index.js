0c4950b787332df5a2d5af26c50905d37857ad0f
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-09-21 18:14:43 +0200

    Export schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export * from "./src/types.js";
 export { AggregateValidationError } from "./src/errors/aggregate-validaton-error.js";
 export { ValidationError } from "./src/errors/validation-error.js";
+export { Schema } from "./src/schemas/schema.js";
 export { BooleanSchema } from "./src/schemas/boolean.js";
 export { NumberSchema } from "./src/schemas/number.js";
 export { StringSchema } from "./src/schemas/string.js";
