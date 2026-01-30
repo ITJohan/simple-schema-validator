4469d4dcb47ca666b41dbc8a4514e15d00c4c331
Johan Lindkvist <johanlindkvist89@gmail.com>
2026-01-30 21:38:04 +0100

    feat: email function

--- src/schemas/string.js
+++ src/schemas/string.js
@@ -2,38 +2,54 @@
  * @param {object} [options]
  * @param {string} options.message
  */
-export const string = ({message} = {message: 'Not a string'}) => {
+export const string = ({ message } = { message: "Not a string" }) => {
 	/** @type {((x: string) => string | undefined)[]} */
 	const checks = [];
-	
+
 	const schema = {
 		/**
-		 * @param {number} min 
+		 * @param {number} min
 		 * @param {object} [options]
 		 * @param {string} options.message
 		 */
-		min: (min, {message} = { message: `Minimum string length is ${min}` }) => {
-      checks.push((x) => (x.length < min ? message : undefined));
-      return schema;
-    },
+		min: (
+			min,
+			{ message } = { message: `Minimum string length is ${min}` },
+		) => {
+			checks.push((x) => (x.length < min ? message : undefined));
+			return schema;
+		},
 		/**
-		 * @param {unknown} x 
+		 *
+		 * @param {object} [options]
+		 * @param {string} options.message
 		 */
-    parse: (x) => {
-			if (typeof x !== 'string') return {data: x, errors: [message]}
+		email: ({ message } = { message: "Not an email" }) => {
+			checks.push((x) =>
+				!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(x)
+					? message
+					: undefined,
+			);
+			return schema;
+		},
+		/**
+		 * @param {unknown} x
+		 */
+		parse: (x) => {
+			if (typeof x !== "string") return { data: x, errors: [message] };
 
 			/** @type {string[]} */
 			const errors = [];
-      
-      for (const check of checks) {
-        const error = check(x);
-        if (error) errors.push(error);
-      }
 
-      if (errors.length > 0) return { data: x, errors };
-      return { data: x, errors: undefined };
-    }
-	}
+			for (const check of checks) {
+				const error = check(x);
+				if (error) errors.push(error);
+			}
 
-	return schema
-}
+			if (errors.length > 0) return { data: x, errors };
+			return { data: x, errors: undefined };
+		},
+	};
+
+	return schema;
+};
--- src/schemas/string.test.js
+++ src/schemas/string.test.js
@@ -5,31 +5,90 @@ import { string } from "./string.js";
 describe(string.name, () => {
 	it("should validate a string", () => {
 		deepStrictEqual(string().parse("abc").data, "abc");
+		deepStrictEqual(string().parse("abc").errors, undefined);
 		deepStrictEqual(string().parse("").data, "");
+		deepStrictEqual(string().parse("").errors, undefined);
 	});
 
 	it("should invalidate a non-string", () => {
-		deepStrictEqual(string({message: "Not a string"}).parse(1).errors, ['Not a string']);
-		deepStrictEqual(string({message: "Not a string"}).parse(true).errors, ['Not a string']);
-		deepStrictEqual(string({message: "Not a string"}).parse({}).errors, ['Not a string']);
-		deepStrictEqual(string({message: "Not a string"}).parse(() => {}).errors, ['Not a string']);
+		deepStrictEqual(string().parse(1).data, 1);
+		deepStrictEqual(string({ message: "Not a string" }).parse(1).errors, [
+			"Not a string",
+		]);
+		deepStrictEqual(string().parse(true).data, true);
+		deepStrictEqual(string({ message: "Not a string" }).parse(true).errors, [
+			"Not a string",
+		]);
+		deepStrictEqual(string().parse({}).data, {});
+		deepStrictEqual(string({ message: "Not a string" }).parse({}).errors, [
+			"Not a string",
+		]);
+		deepStrictEqual(
+			string({ message: "Not a string" }).parse(() => {}).errors,
+			["Not a string"],
+		);
 	});
 
-	// describe("email", () => {
-	// 	it("should validate a valid email", () => {
-	// 		deepStrictEqual(
-	// 			new string().email().parse("john@doe.com"),
-	// 			"john@doe.com",
-	// 		);
-	// 	});
+	describe("min", () => {
+		it("should validate a longer string", () => {
+			deepStrictEqual(string().min(5).parse("123456").data, "123456");
+			deepStrictEqual(string().min(5).parse("123456").errors, undefined);
+		});
 
-	// 	it("should invalidate an unvalid email", () => {
-	// 		throws(() => new string().email().parse("john@doe."));
-	// 		throws(() => new string().email().parse("john@doe"));
-	// 		throws(() => new string().email().parse("johndoe.com"));
-	// 		throws(() => new string().email().parse("@doe.com"));
-	// 		throws(() => new string().email().parse("john"));
-	// 		throws(() => new string().email().parse(""));
-	// 	});
-	// });
+		it("should validate a equal length string", () => {
+			deepStrictEqual(string().min(5).parse("12345").data, "12345");
+			deepStrictEqual(string().min(5).parse("12345").errors, undefined);
+		});
+
+		it("should invalidate a shorter string", () => {
+			deepStrictEqual(string().min(5).parse("1234").data, "1234");
+			deepStrictEqual(
+				string().min(5, { message: "Too short" }).parse("1234").errors,
+				["Too short"],
+			);
+		});
+	});
+
+	describe("email", () => {
+		it("should validate a valid email", () => {
+			deepStrictEqual(
+				string().email().parse("john@doe.com").data,
+				"john@doe.com",
+			);
+			deepStrictEqual(string().email().parse("john@doe.com").errors, undefined);
+		});
+
+		it("should invalidate an unvalid email", () => {
+			deepStrictEqual(string().email().parse("john@doe.").data, "john@doe.");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("john@doe.").errors,
+				["Not an email"],
+			);
+			deepStrictEqual(string().email().parse("john@doe").data, "john@doe");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("john@doe").errors,
+				["Not an email"],
+			);
+			deepStrictEqual(string().email().parse("johndoe.com").data, "johndoe.com");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("johndoe.com").errors,
+				["Not an email"],
+			);
+			deepStrictEqual(string().email().parse("@doe.com").data, "@doe.com");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("@doe.com").errors,
+				["Not an email"],
+			);
+			deepStrictEqual(string().email().parse("john").data, "john");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("john").errors,
+				["Not an email"],
+			);
+			deepStrictEqual(string().email().parse("").data, "");
+			deepStrictEqual(
+				string().email({ message: "Not an email" }).parse("").errors,
+				["Not an email"],
+			);
+		});
+	});
 });
